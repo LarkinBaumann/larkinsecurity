53fc2914aed24bde1c5a0c9ba9c97debe2ba20f7
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-03-22 03:55:44 -0600

    Refactor Servicio component layout

--- src/components/Servicio/Servicio.jsx
+++ src/components/Servicio/Servicio.jsx
@@ -4,19 +4,31 @@ import React from 'react'
 function Servicio() {
   return (
     <div className='text-white w-full h-full flex flex-col'>
-      <div className='w-full h-full flex flex-row justify-center '>
-        <div className='h-full flex flex-col justify-end'>
-          <Image src="/assets/servicio/escudo1.png" alt="Escudo" width={1000} height={1000} className='w-[123px] h-[126px]' />
+      <div className='w-full h-full flex flex-row justify-center px-4 '>
+        <div className='flex flex-col justify-end relative '>
+          <Image src="/assets/servicio/escudo1.png" alt="Escudo" width={1000} height={1000} className='w-full h-[126px]' />
+          <div className='flex flex-col text-center w-full absolute top-[56%] text-[12px] font-paragraph leading-[15px]'>
+          <p className='font-extrabold'>Visión global</p>
+          <p className='font-light'>e innovación</p>
+          </div>
         </div>
-        <div className='h-full flex flex-col justify-start'>
+        <div className='h-full flex flex-col justify-start relative'>
           <Image src="/assets/servicio/escudo1.png" alt="Escudo" width={1000} height={1000}
-          className='w-[182px] h-[197px]'
+          className='w-full h-[197px]'
           />
+          <div className='flex flex-col text-center w-full absolute top-[35%] text-[14px] font-paragraph leading-[15px]'>
+          <p className='font-extrabold'>Compromiso con</p>
+          <p className='font-light'>la excelecia</p>
+          </div>
         </div>
-        <div className='h-full flex flex-col justify-end'>
+        <div className=' flex flex-col justify-end relative'>
           <Image src="/assets/servicio/escudo1.png" alt="Escudo" width={1000} height={1000} 
-          className='w-[123px] h-[126px]'
+          className='w-full h-[126px]'
           />
+           <div className='flex flex-col text-center w-full absolute top-[56%] text-[12px] font-paragraph leading-[15px]'>
+          <p className='font-extrabold'>Integridad y</p>
+          <p className='font-light'>Confianza</p>
+          </div>
         </div>
 
       </div>
