82648c2e5d0f62082603a00015d8df650b0af5b6
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-03-23 05:37:59 -0600

    Refactor Acerca component to include images and shadows

--- src/components/Home/Acerca/Acerca.jsx
+++ src/components/Home/Acerca/Acerca.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import Button from '../../Button'
+import Image from 'next/image'
+import ShadowTop from './Shadows/ShadowTop'
 
 function Acerca() {
   return (
-    <div className='w-full h-full flex flex-col items-center justify-center'>
-        <div>
-            <span>Imagenes</span>
+    <div className='w-full h-full flex flex-col items-center justify-center relative'>
+      <ShadowTop/>
+
+      <div className='w-full h-[300px] flex flex-row justify-center items-center relative'>
+
+        <div className='w-full h-[350px] absolute left-[0%] top-[35px] z-0'>
+            <Image src='/assets/home/acerca/fondo.png' width={2000} height={2000} alt='Acerca de nosotros' className='w-full h-full' />
+        </div>
+        <div className='w-[222px] h-[189px] absolute left-[5%] z-20'>
+            <Image src='/assets/home/acerca/1.png' width={1920} height={1080} alt='Acerca de nosotros' className='w-full h-full' />
+        </div>
+        <div className='w-[222px] h-[189px] absolute right-[5%] z-10'>
+            <Image src='/assets/home/acerca/2.png' width={1920} height={1080} alt='Acerca de nosotros' className='w-full h-full' />
+        </div>
         </div>
         <div className='w-full h-full flex flex-col items-center justify-center  text-white text-center gap-[15px]'>
-            <h2 className='font-header text-[30px] font-black leading-[32px] w-[95%] '>
+            <h2 className='font-header text-[30px] font-black leading-[32px] w-[95%] z-10 '>
                 Garantizamos tu tranquilidad con seguridad de <span className='titulo3'>primera calidad</span>
             </h2>
             <p>
---
+++ src/components/Home/Acerca/Shadows/ShadowTop.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function ShadowTop() {
+  return (
+    <div className='absolute -top-[250px] z-10'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="428" height="552" viewBox="0 0 428 552" fill="none">
+    <g filter="url(#filter0_f_415_673)">
+      <path d="M1435.96 257.709C1122.07 155.618 40.5876 131.406 -460.92 132.061C-1784.89 287.08 -148.073 388.469 835.831 419.786C1166.66 408.298 1749.84 359.799 1435.96 257.709Z" fill="#020202"/>
+    </g>
+    <defs>
+      <filter id="filter0_f_415_673" x="-1088.89" y="0.14859" width="2746.63" height="551.537" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+        <feGaussianBlur stdDeviation="65.95" result="effect1_foregroundBlur_415_673"/>
+      </filter>
+    </defs>
+  </svg></div>
+  )
+}
+
+export default ShadowTop
