10f3797a2a90e9e1514e80b63cab21caaf582f7b
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-03-22 20:19:46 -0600

    Update styles and layout in Valores component

--- src/components/Acerca/Valores/Valores.jsx
+++ src/components/Acerca/Valores/Valores.jsx
@@ -4,7 +4,7 @@ import ValoresCard from './ValoresCard'
 function Valores() {
   return (
     <div
-    className='w-full h-full flex flex-col items-center justify-center text-white px-4  '
+    className='w-full h-full flex flex-col items-center justify-center text-white px-4 mt-8 '
     >
     
     <h2 className='text-[25px] font-header font-extrabold'>
@@ -13,7 +13,7 @@ function Valores() {
     <p className='text-[9px] font-paragraph font-semiblack leadong-[15px] text-center'>
     Basados en los siguientes valores nos comprometemos a seguir siendo un referente en seguridad y protección para aquellos que confían en nosotros.
     </p>
-    <div className='w-full h-full flex flex-row justify-center items-center'>
+    <div className='w-full h-full flex flex-row justify-center items-center gap-[11px] mt-8'>
         <ValoresCard
         icono={'/assets/icons/innovacion.png'}
         titulo={'Visión Global e'}
@@ -21,13 +21,13 @@ function Valores() {
         />
         <ValoresCard
         icono={'/assets/icons/compromiso.png'}
-        titulo={'Visión Global e'}
-        parrafo={'Innovacion'}
+        titulo={'Compromiso con'}
+        parrafo={'Excelencia'}
         />
         <ValoresCard
         icono={'/assets/icons/integridad.png'}
-        titulo={'Visión Global e'}
-        parrafo={'Innovacion'}
+        titulo={'Integridad y'}
+        parrafo={'Confianza'}
         />
     </div>
     </div>
