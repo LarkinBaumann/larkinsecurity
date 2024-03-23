1e338420b2e932348dd80faeb2e4d0a507244eb9
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-03-23 07:34:21 -0600

    Add hero shadow image and adjust positioning

--- src/components/Acerca/Hero/Hero.jsx
+++ src/components/Acerca/Hero/Hero.jsx
@@ -4,8 +4,9 @@ import React from 'react'
 function Hero() {
   return (
     <div
-    className='text-white w-full h-screen flex flex-col items-center justify-center gap-[55px]'
+    className='text-white w-full h-screen flex flex-col items-center justify-center gap-[55px] relative'
     >
+      <Image src="/assets/acerca/hero/heroShadow.svg" width={1000} height={1000} className='w-full h-full absolute top-[80px] ' alt='fondo acerca'/>
       <div className='w-full flex flex-col items-center text-center'>
         <h2 className='text-[38px] font-header font-black leading-[42px]'>
           Tu seguridad es
@@ -21,8 +22,8 @@ function Hero() {
         </h2>
 
       </div>
-      <div>
-        <Image src="/assets/acerca/hero/hero.png" width={1000} height={1000} alt="Hero Acerca" className='w-[295px] h-[295px]' />
+      <div className='z-10'>
+        <Image src="/assets/acerca/hero/hero.png" width={1000} height={1000} alt="Hero Acerca" className='w-[295px] h-[295px] ' />
       </div>
       
     </div>
