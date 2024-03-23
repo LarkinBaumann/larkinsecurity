903a95ae5e511ac5cfc0d17b6855c39fcb44b390
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-03-23 17:40:13 -0600

    Update page title in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
     <>
     <Head>
     <meta name="theme-color" content="#050409"/>
-    <title>Larkin Baumann Holding.</title>
+    <title>Larkin Baumann Security.</title>
     <meta name="description" content="Descubre Larkin Baumann Holding, empresa líder en diversas industrias a nivel global desde 1997. Nuestra holding se especializa en la creación y gestión de empresas en sectores que van desde la seguridad privada hasta la construcción de viviendas. Con un enfoque en la excelencia y la responsabilidad, somos la elección confiable para tus necesidades empresariales." />
     <meta name="keywords" content="seguridad privada, seguridad, finanzas, metales preciosos, oro, metales, hierro, viviendas, mineria, real estate,  petroleo, gas, inversion, inversiones, industrial, industria, proteccion, gestion financiera, diesel, propiedades, escoltas" />
     <meta property="og:title" content="Larkin Baumann Holding" />
