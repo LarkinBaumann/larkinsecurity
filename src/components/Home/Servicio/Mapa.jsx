1418a53fe3bcdfe203e6dded95c466a24bca2e23
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-03-25 05:02:20 -0600

    Update Mapa component styling

--- src/components/Home/Servicio/Mapa.jsx
+++ src/components/Home/Servicio/Mapa.jsx
@@ -6,9 +6,9 @@ function Mapa() {
     <div className='w-full h-full px-4 '>
 
     <div className='w-full h-[502px] flex flex-col lg:flex-row justify-center items-center text-center lg:text-left rounded-[22px] border-[2px] border-[#1777BA] bg-[#07141C]
-    gap-[15px] 
+    gap-[15px] lg:px-8
     '>
-        <div className='w-[90%] md:h-[100px]  flex flex-col items  '>
+        <div className='w-[90%] md:h-[100px] lg:w-[60%] lg:h-[260px]  flex flex-col   '>
         <h2 className='h-full font-header text-[22px] md:text-[40px] font-extrabold leading-[25px] md:leading-[40px]  '>
             Tus Socios en Todo México:
             <br/>
@@ -17,13 +17,13 @@ function Mapa() {
             </span>
         </h2>
 
-        <div className='w-[90%] hidden lg:flex flex-row justify-center font-paragraph text-[12px] md:text-[20px] leading-[20px] md:leading-[34px] '>
+        <div className='w-[90%] lg:w-[490px] hidden lg:flex flex-row justify-center font-paragraph text-[12px] md:text-[20px] lg:text-[18px] leading-[20px] md:leading-[34px] lg:leading-[30px] '>
         Cobertura en toda la República Mexicana,  nuestro equipo está listo para atender tus necesidades donde sea que estés. Confía en nosotros para proporcionarte soluciones confiables y de calidad en todo el país.
         </div>
         
         </div>
-        <div className='w-[90%] flex flex-row justify-center'>
-            <Image src="/assets/home/servicios/mapa.png" alt="Mapa" width={1000} height={1000} className='w-[237px] h-[229px] animate-pulse' />
+        <div className='w-[90%] lg:w-[40%] flex flex-row justify-center'>
+            <Image src="/assets/home/servicios/mapa.png" alt="Mapa" width={1000} height={1000} className='w-[237px] lg:w-[406px] h-[229px] lg:h-[393px] animate-pulse' />
         </div>
         <div className='w-[90%] flex lg:hidden flex-row justify-center font-paragraph text-[12px] md:text-[20px] leading-[20px] md:leading-[34px] '>
         Cobertura en toda la República Mexicana,  nuestro equipo está listo para atender tus necesidades donde sea que estés. Confía en nosotros para proporcionarte soluciones confiables y de calidad en todo el país.
