3136a8f2b9c1a5c60647247af1ffaac86e954e69
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-03-25 19:00:56 -0600

    Update Valores component styles and add text to ValoresCard

--- src/components/Acerca/Valores/Valores.jsx
+++ src/components/Acerca/Valores/Valores.jsx
@@ -5,14 +5,14 @@ import Shadow from './Shadows/Shadow'
 function Valores() {
   return (
     <div
-    className='w-full h-[250px] md:h-[400px] flex flex-col items-center justify-center text-white px-4 mt-8 relative z-10 '
+    className='w-full h-[250px] md:h-[400px] lg:h-[800px] flex flex-col items-center justify-center text-white px-4 lg:px-10 mt-8 relative z-10 '
     >
       <Shadow/>
     <div className='flex flex-col justify-center items-center'>
-    <h2 className='text-[25px] md:text-[40px] font-header font-extrabold'>
+    <h2 className='text-[25px] md:text-[40px] lg:text-[60px] font-header font-extrabold'>
         Valores
     </h2>
-    <p className='text-[9px] md:text-[20px]  font-paragraph font-semiblack leading-[15px]  md:leading-[34px]  text-center'>
+    <p className='text-[9px] md:text-[20px]  font-paragraph font-semiblack leading-[15px]  md:leading-[34px] lg:leading-[30px]  text-center lg:px-[150px]'>
     Basados en los siguientes valores nos comprometemos a seguir siendo un referente en seguridad y protección para aquellos que confían en nosotros.
     </p>
   
@@ -23,16 +23,21 @@ function Valores() {
         icono={'/assets/icons/innovacion.png'}
         titulo={'Visión Global e'}
         parrafo={'Innovacion'}
+        texto = {"Nos impulsamos hacia nuevas ideas y enfoques para abordar los desafíos de seguridad en un mundo cambiante. Estamos comprometidos con la innovación y la adopción de tecnologías emergentes para adaptarnos rápidamente a las necesidades globales de seguridad."}
         />
         <ValoresCard
         icono={'/assets/icons/compromiso.png'}
         titulo={'Compromiso con'}
         parrafo={'Excelencia'}
+        texto={"Buscamos la excelencia en cada aspecto de nuestro trabajo, desde la selección y formación de nuestro equipo hasta la implementación de tecnología, con el objetivo de superar expectativas y establecer estándares de calidad excepcionales en todos nuestros servicios."}
         />
         <ValoresCard
         icono={'/assets/icons/integridad.png'}
         titulo={'Integridad y'}
         parrafo={'Confianza'}
+        texto={
+          "Nos comprometemos a actuar con honestidad, transparencia y ética en todas nuestras interacciones. Nuestros clientes confían en nosotros para proteger lo que más valoran. En cada servicio nuestro objetivo es mantener esa confianza con integridad y excelencia constante."
+        }
         />
     </div>
     </div>
--- src/components/Acerca/Valores/ValoresCard.jsx
+++ src/components/Acerca/Valores/ValoresCard.jsx
@@ -1,17 +1,23 @@
 import Image from 'next/image'
 import React from 'react'
 
-function ValoresCard({titulo, parrafo,icono}) {
+function ValoresCard({titulo, parrafo,icono, texto}) {
   return (
-    <div className='w-[103px] h-[108px] rounded-[6px] border-[0.3px] border-[#55B0F0] bg-[#0E1921] flex flex-col gap-[4px] justify-center items-center'>
-          <div className='w-[53px] h-[53px]'>
-          <Image  src={icono} alt="Email" width={100} height={100} className='w-[56px] h-[53px] object-contain ' />
+    <div className={`w-[103px] lg:w-[359px] h-[108px] lg:h-[494px] rounded-[6px] border-[0.3px] border-[#55B0F0] bg-[#0E1921] flex flex-col gap-[4px] justify-center items-center lg:rounded-[18px]
+    ${parrafo == 'Excelencia' && "lg:translate-y-[50px]"}
+    `}>
+          <div className='w-[53px] lg:w-[105px] h-[53px] lg:h-[152px]'>
+          <Image  src={icono} alt="Email" width={100} height={100} className='w-full h-full object-contain  ' />
           </div>
           
-          <div className='w-full flex flex-col'>
-
-          <h4 className='text-[12px] text-center font-semibold font-paragraph'>{titulo}</h4>
-          <p className='text-[12px] text-center font-paragraph '>{parrafo}</p>
+          <div className='w-full flex flex-col lg:gap-[21px]'>
+            <div>
+          <h4 className='text-[12px] lg:text-[21px] text-center font-semibold font-paragraph'>{titulo}</h4>
+          <p className='text-[12px] lg:text-[21px] text-center font-paragraph '>{parrafo}</p>
+            </div>
+          <p className='hidden lg:block text-[15px]  text-center leading-[25px] font-extrabold px-4 font-paragraph'>
+            {texto}
+          </p>
           </div>
         </div>
   )
